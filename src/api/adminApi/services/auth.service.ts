cfebe762bc752f6d7cd66b4421b2a097d3af1167
IyiolaJay <jimoh.iyiola11@gmail.com>
2024-12-05 17:58:58 +0100

    Cached new establishment id

--- src/api/adminApi/services/auth.service.ts
+++ src/api/adminApi/services/auth.service.ts
@@ -10,30 +10,40 @@ import SecurityHelperService from "../../../helpers/security";
 import { generateRandomPassword } from "../../../helpers/password";
 import EmailService from "../../../email/emailer";
 import { EmailType } from "../../../utilities/enums/enum";
-import { IClient, IClientRepository } from "../../../interfaces/client.interface";
+import {
+  IClient,
+  IClientRepository,
+} from "../../../interfaces/client.interface";
 import { mapPermisionValuesToKeys } from "../../../helpers/permissions.mapper";
-import { IActiveService, IServiceRepository } from "../../../interfaces/service.interface";
+import {
+  IActiveService,
+  IServiceRepository,
+} from "../../../interfaces/service.interface";
+import { cacheData, getCacheData } from "../../../redis/redis.cache";
 
 export default class AdminAuthService {
   private AdminRepository: IAdminRepository;
-  private ServiceRepository : IServiceRepository;
-  private securityHelperService: SecurityHelperService = new SecurityHelperService();
+  private ServiceRepository: IServiceRepository;
+  private securityHelperService: SecurityHelperService =
+    new SecurityHelperService();
   // private otpRepository: IOtpRepository;
   private emailService = new EmailService();
   private ClientRepository: IClientRepository;
 
-
-  constructor(adminRepository: IAdminRepository, clientRepository: IClientRepository, serviceRepository : IServiceRepository) {
+  constructor(
+    adminRepository: IAdminRepository,
+    clientRepository: IClientRepository,
+    serviceRepository: IServiceRepository
+  ) {
     this.AdminRepository = adminRepository;
     this.ServiceRepository = serviceRepository;
     this.ClientRepository = clientRepository;
-
   }
 
   /**
-   * 
-   * @param admin 
-   * @returns 
+   *
+   * @param admin
+   * @returns
    */
   async CreateAdminAccount(admin: IAdmin) {
     let _admin = await this.AdminRepository.findOneByFilter({
@@ -46,9 +56,9 @@ export default class AdminAuthService {
         "Admin account exists with this email"
       );
 
-      admin.permissionSet = mapPermisionValuesToKeys(admin.permissionSet)
-    
-      const genPassword = generateRandomPassword();
+    admin.permissionSet = mapPermisionValuesToKeys(admin.permissionSet);
+
+    const genPassword = generateRandomPassword();
 
     _admin = await this.AdminRepository.create({
       ...admin,
@@ -58,31 +68,35 @@ export default class AdminAuthService {
     //send credentials to admin mail
     this.emailService.SendEMailToUser(
       {
-        to : admin.email,
-        bodyParts : {
-          name : _admin.name,
-          email : _admin.email,
-          password : genPassword,
-          _id : _admin._id
-          },
+        to: admin.email,
+        bodyParts: {
+          name: _admin.name,
+          email: _admin.email,
+          password: genPassword,
+          _id: _admin._id,
+        },
       },
       EmailType.CredentialsEmail
-    )
+    );
 
     return;
   }
 
   /**
-   * 
-   * @param client 
-   * @returns 
+   *
+   * @param client
+   * @returns
    */
-  async CreateClientAccount(client: IClient, creatorId : string, defaultService? : Partial<IActiveService>) {
+  async CreateClientAccount(
+    client: IClient,
+    creatorId: string,
+    defaultService?: Partial<IActiveService>
+  ) {
     let _client = await this.ClientRepository.findOneByFilter({
       email: { $regex: new RegExp(`^${client.email}$`, "i") },
     });
 
-    if (_client){
+    if (_client) {
       throw new ApiError(
         httpStatus.CONFLICT,
         "Client account exists with this email"
@@ -100,27 +114,39 @@ export default class AdminAuthService {
     //send credentials to client mail
     this.emailService.SendEMailToUser(
       {
-        to : _client.email,
-        bodyParts : {
-          name : _client.name,
-          email : _client.email,
-          password : genPassword,
-          _id : _client._id
-          },
+        to: _client.email,
+        bodyParts: {
+          name: _client.name,
+          email: _client.email,
+          password: genPassword,
+          _id: _client._id,
+        },
       },
       EmailType.CredentialsEmail
-    )
+    );
 
-    if(defaultService){
+    if (defaultService) {
       await this.ServiceRepository.createActiveService({
-        clientId : _client.clientId,
-        serviceId : defaultService.serviceId as string,
-        expireDate : defaultService.expireDate as Date,
-      })
+        clientId: _client.clientId,
+        serviceId: defaultService.serviceId as string,
+        expireDate: defaultService.expireDate as Date,
+      });
+
+      const cachedEstablishment = JSON.parse(
+        (await getCacheData("acs_01")) ?? "[]"
+      );
+
+      //caching data for realtime middleware service
+      // this block will add the new user establishment IDs for 
+      await cacheData({
+        key: "acs_01",
+        value: JSON.stringify([
+          ...cachedEstablishment,
+          ...client.establishmentId,
+        ]),
+      });
     }
 
     return;
   }
 }
-
-
