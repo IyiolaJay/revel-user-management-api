9da0482b7c8dc10f90d554a9438c17de29e3b9eb
IyiolaJay <jimoh.iyiola11@gmail.com>
2024-12-20 12:10:30 +0100

    Add check

--- src/api/adminApi/validators/auth.validators.ts
+++ src/api/adminApi/validators/auth.validators.ts
@@ -3,44 +3,50 @@ import RequestValidator from "../../../middlewares/schema.middleware";
 import { Permissions } from "../../../utilities/enums/permissions.enum";
 
 const createAdmin = RequestValidator.requestItemsStructure({
-    email : Joi.string().email().required(),
-    name : Joi.string().required(),
-    permissionSet : Joi.array().items(Joi.string().valid(...Object.values(Permissions)).invalid(Permissions.ALL))
-})
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  permissionSet: Joi.array().items(
+    Joi.string()
+      .valid(...Object.values(Permissions))
+      .invalid(Permissions.ALL)
+  ),
+});
 
 const loginAdmin = RequestValidator.requestItemsStructure({
-    email : Joi.string().email().required(),
-    password : Joi.string().required(),
-})
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
 
 const verifyToken = RequestValidator.requestItemsStructure({
-    token : Joi.string().required(),
-})
+  token: Joi.string().required(),
+});
 
 const changePassword = RequestValidator.requestItemsStructure({
-    password : Joi.string().required(),
-})
+  password: Joi.string().required(),
+});
 
 const changePasswordQuery = RequestValidator.requestItemsStructure({
-    userId : Joi.string().required(),
-})
+  userId: Joi.string().required(),
+});
 
 const createClient = RequestValidator.requestItemsStructure({
-    email : Joi.string().email().required(),
-    name : Joi.string().required(),
-    establishmentId : Joi.array().items(Joi.number()).required(),
-    establishmentUrl : Joi.string().required(),
-    subscribedService : Joi.array().items(
-        Joi.string().required()
-    )
-})
-
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  establishmentId: Joi.array().items(Joi.number()).required(),
+  establishmentUrl: Joi.string()
+    .uri({ scheme: ["http", "https"] })
+    .required()
+    .custom((value, _) => {
+        return value.replace(/\/$/, ''); // Removes trailing slash
+    }),
+  subscribedService: Joi.array().items(Joi.string().required()),
+});
 
 export default {
-    createAdmin,
-    createClient,
-    loginAdmin,
-    verifyToken,
-    changePassword,
-    changePasswordQuery
-}
+  createAdmin,
+  createClient,
+  loginAdmin,
+  verifyToken,
+  changePassword,
+  changePasswordQuery,
+};
